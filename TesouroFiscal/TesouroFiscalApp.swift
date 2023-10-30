fec3673f8abc345968b21258ac7955212de63527
Giovanna Micher Santana <111765178+citricgio@users.noreply.github.com>
2023-10-30 14:04:25 -0300

    Initial Commit

---
+++ TesouroFiscal/TesouroFiscalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesouroFiscalApp.swift
+//  TesouroFiscal
+//
+//  Created by Giovanna Micher on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TesouroFiscalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
